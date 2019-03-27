92e929a2c956703c4eea70fe69148b0cd3f46d47
Tony Baker <this.tonybaker@gmail.com>
2019-03-27 20:25:28 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,7 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
-    #BRANDS << brand
+    BRANDS << brand
     if BRANDS.any?{|existing_brand| brand == existing_brand}
       BRANDS << brand
     end
